d7d6e3b6682d1909530a86b76d5c18161ac793d3
Ardian Pradana <ardianysp18@gmail.com>
2024-01-20 01:51:13 +0700

    Create check winner

--- main.js
+++ main.js
@@ -1,13 +1,81 @@
+// SETUP
 const Setup = (() => {
   const board = ["", "", "", "", "", "", "", "", ""];
   const boardContaner = document.querySelector(".board");
   const playerName = document.querySelector(".player");
   const playerBoard = document.querySelector(".player-info");
-  let status = false;
+  let gameOver = false;
+  const playerDisplay = document.querySelector(".player-display");
+  const winning = document.querySelector(".winning");
+  const playing = document.querySelector(".playing");
+
+  const checkWinner = (name, mark) => {
+    const keys = [
+      // Rows
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+
+      // Columns
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+
+      // Diagonals
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    let choices = [];
+
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] === mark) {
+        choices.push(i);
+      }
+    }
+
+    for (let i = 0; i < keys.length; i++) {
+      let key = keys[i];
+
+      if (key.every(box => choices.includes(box))) {
+        gameOver = true;
+        winning.textContent = `The winner is ${name}`;
+        winning.style.display = "block";
+        playing.style.display = "none";
+        // playerDisplay.textContent = `The winner is ${name}`;
+      }
+    }
+  };
+
+  const comChoice = () => {
+    let compArray = [];
+
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] === "") {
+        compArray.push(i);
+      }
+    }
+
+    let idx = Math.floor(Math.random() * compArray.length);
+    let chosenIdx = compArray[idx];
+
+    board[chosenIdx] = "O";
+    render();
+  };
 
   const singlePlayer = player => {
     boardContaner.addEventListener("click", event => {
-      console.log(player);
+      let clickBtn = parseInt(event.target.className.split("-")[1]);
+
+      if (!gameOver && board[clickBtn] === "") {
+        board[clickBtn] = "X";
+        render();
+        checkWinner(player[0].name, player[0].mark);
+        if (!gameOver) {
+          comChoice();
+          checkWinner(player[1].name, player[1].mark);
+        }
+      }
     });
   };
 
@@ -40,8 +108,10 @@ const createPlayer = (name, mark) => {
   };
 };
 
+// GAME
 const Game = (() => {
   const player1 = document.querySelector("#player");
+
   const updatePlayer = currentPlayers => {
     document.querySelector(
       ".player1"
@@ -54,17 +124,13 @@ const Game = (() => {
 
   const start = () => {
     const players = [
-      createPlayer(
-        player1.value,
-
-        "X"
-      ),
+      createPlayer(player1.value === "" ? "Player 1" : player1.value, "X"),
       createPlayer("Computer", "O")
     ];
     updatePlayer(players);
 
     Setup.render();
-    Setup.singlePlayer();
+    Setup.singlePlayer(players);
   };
 
   return {
